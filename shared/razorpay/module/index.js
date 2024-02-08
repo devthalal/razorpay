d1fa464cf9d081c9e6b278fec9169a0ead43b526
devthalal <thalal.hassan@neoito.com>
2024-02-08 15:43:27 +0530

    user middleware

--- shared/razorpay/module/index.js
+++ shared/razorpay/module/index.js
@@ -2,11 +2,11 @@ import Razorpay from 'razorpay'
 import vault from '../../vault/index.js'
 
 const getRazorpayInstance = async (req) => {
-  const { user } = req.headers
+  const { id } = req.user
 
   await vault.healthCheck()
 
-  const readData = await vault.readKVSecret(process.env.VAULT_TOKEN, user)
+  const readData = await vault.readKVSecret(process.env.VAULT_TOKEN, id)
   const config = readData.data
 
   const razorpayInstance = new Razorpay(config)
